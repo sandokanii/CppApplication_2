00ac6ae8b927fc3f201cd0a93984533c88ee8dfb
sandokanii <roberto_calisto@hotmail.com>
2017-06-30 15:28:55 -0300

    ultimo tok
    asdd

--- proveedores.c
+++ proveedores.c
@@ -9,30 +9,45 @@
 
 void AltaProvs(){
     FILE *pf;
-    Proveedores  proveedor;
-    pf = fopen("proveedores.dat","a");
+    Proveedores proveedor;
+    int band =1;
+    pf = fopen("Proveedores.dat", "a");  
+    do {
     printf("Ingrese Codigo\n");
-    scanf("%i",proveedor.codigo);
+    scanf("%i", &proveedor.codigo);
+    fflush(stdin);
+     //while (!feof(pf)
+    
+                if (proveedor.codigo != proveedor.codigo){
+                                        
+                    band=0;
+                }else{
+                    printf("Re-ingrese nuevo codigo\n");
+                    band=1;
+                }
+    }while((band != 0));
     printf("Ingrese Nombre\n");
     fflush(stdin);
-    scanf("%s",&proveedor.nombre);
-    fflush(stdin);
-    printf("Ingrese Telefono\n");
-    scanf("%s",&proveedor.telefono);
-    fflush(stdin);
+    scanf("%s", proveedor.nombre);
     printf("Ingrese Email\n");
-    scanf("%s",&proveedor.email);
-    fseek(pf,0L,SEEK_END);
-    fwrite(&proveedor,sizeof(Proveedores),1,pf);
+    fflush(stdin);
+    scanf("%s", proveedor.email);    
+    printf("Ingrese Telefono\n");
+    fflush(stdin);
+    scanf("%s", proveedor.telefono);
+        
+     
+    fseek(pf, 0L, SEEK_END);
+    fwrite(&proveedor, sizeof (Proveedores), 1, pf);
     fclose(pf);
     system("clear");
-   
+        
  }
 void BajaProvs(){
     FILE *pf,*pfaux;
     Proveedores proveedor;
     int codigoaux;
-    pf = fopen("Provedores.dat","r");
+    pf = fopen("Proveedores.dat","r");
     pfaux = fopen("Proveedoresaux.dat","a");
     printf("Ingrese Codigo\n");
     scanf("%i",&codigoaux);
@@ -53,30 +68,40 @@ void ModifProvs(){
     FILE *pf,*pfaux;
     Proveedores proveedor;
     int codigoaux;
+    int opcion;
     pf = fopen("Provedores.dat","r");
     pfaux = fopen("Proveedoresaux.dat","a");
     printf("Ingrese Codigo\n");
     scanf("%i",&codigoaux);
-    fread(&proveedor,sizeof(Proveedores),1,pf);
+    fread(&proveedor, sizeof (Proveedores), 1, pf);
         while (!feof(pf)){
                 if (proveedor.codigo != codigoaux){
                     fseek(pfaux,0l,SEEK_END);
                     fwrite(&proveedor,sizeof(Proveedores),1,pfaux);
                 }else{
+                    printf("Ingrese  opcion a modificar: 1 Nombre, 2 Telefono, 3 Email");
+                    scanf("%i",opcion);
+                    switch (opcion){
+                        case 1:
                     printf("Ingrese Nombre\n");
                     fflush(stdin);
                     scanf("%s",proveedor.nombre);
+                    break;
+                        case 2:
                     printf("Ingrese Telefono\n");
                     fflush(stdin);
                     scanf("%s",proveedor.telefono);
+                    break;
+                        case 3:
                     printf("Ingrese Email\n");
                     fflush(stdin);
-                    scanf("%s",proveedor.email);
-                    fflush(stdin);
+                    scanf("%s",proveedor.email);  
+                    break;
+                    }
                     fseek(pfaux,0l,SEEK_END);
-                    fwrite(&proveedor,sizeof(Proveedores),1,pfaux);
+                    fwrite(&proveedor, sizeof (Proveedores), 1,pfaux);
                 }
-            fread(&proveedor,sizeof(Proveedores),1,pf);
+            fread(&proveedor, sizeof (Proveedores), 1,pf);
         }
     fclose(pf);
     fclose(pfaux);
